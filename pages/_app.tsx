c1d57cbcb952e93c005fcd35b09c811475b56ed0
Usama <kusamakhan1234@gmail.com>
2024-12-01 00:29:10 +0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 // pages/_app.tsx
 
-import { AppProps } from "next/app";
+import { AppProps } from "next/app";                              
 import Head from "next/head";
 import "../styles/globals.css"; // Ensure this file includes global resets and utility classes
 
